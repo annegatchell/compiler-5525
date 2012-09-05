6b2ed8a796f371599374604c0c1e7be971a200ad
Anne <moochie@Annes-MacBook-Pro.local>
2012-09-04 23:38:43 -0600

    Added test folder with single test\n Added compile.py, the base file for the compiler. \n compile.py takes a command line arg of a filename and prints the ast.\n

---
+++ compile.py
@@ -0,0 +1,28 @@
+#compile.py
+
+# Anne Gatchell
+# CSCI 5525
+
+"""
+Usage: compile.py <file_name>
+
+"""
+
+import sys
+import compiler
+
+
+def main():
+	if(len(sys.argv) != 2):
+		print 'Usage: compile.py <file_name>'
+		return 1;
+	print 'hello there,', sys.argv[1]
+	inputFile = sys.argv[1]
+	ast = compiler.parseFile(inputFile);
+
+	print ast
+
+
+
+if __name__ == '__main__':
+	main();
