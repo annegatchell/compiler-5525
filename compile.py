5bd9b52cdba4ab9db701e76dbc2060803daf7598
Anne <moochie@econ4-62-223-dhcp.int.colorado.edu>
2012-09-06 18:46:23 -0600

    Making progress on flatten function

--- compile.py
+++ compile.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+
+
 #compile.py
 
 # Anne Gatchell
@@ -7,13 +10,45 @@
 Usage: compile.py <file_name>
 
 """
-
 import sys
 import compiler
+from compiler.ast import *
+
+temp_counter = -1
+
+def temp_gen(basename):
+	global temp_counter
+	temp_counter += 1
+	return basename + str(temp_counter)
+
+
+
+def flatten(ast):
+	if isinstance(ast,Module):
+		#print ast
+		return Module(ast.doc, flatten(ast.node))
+	if isinstance(ast,Stmt):
+		fnodes = map(flatten, ast.nodes)
+		fnodes = sum(fnodes, [])
+		print 'fnodes\n',fnodes
+		return Stmt(fnodes)
+	if isinstance(ast, Printnl):
+		fnodes = map(flatten, ast.nodes)
+		print fnodes
+		print_ = sum(fnodes, [])
+		return Printnl(print_, ast.dest)
+
+	#if isinstance(ast, Name):
+		#print ast
+	#	return(ast,[])
+	#if isinstance(ast, Add):
+		#print ast
 
 
 
 def main():
+	platform = sys.platform
+	print 'Running on a',platform
 	if(len(sys.argv) != 2):
 		print 'Usage: compile.py <file_name>'
 		return 1;
@@ -21,8 +56,9 @@ def main():
 	inputFile = sys.argv[1]
 	ast = compiler.parseFile(inputFile);
 
-	print ast
-
+	print ast, '\n\n\n'
+	fast = flatten(ast)
+	print 'flatten(ast)\n',fast
 
 
 if __name__ == '__main__':
